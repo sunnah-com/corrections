3d12c5560637d460bbaf7b9b1800165f2f7b9ffa
mospy26 <m.mustafaahmed@hotmail.com>
2020-07-29 22:45:59 +1000

    Delete and save to hadith table implemented
    Added back the aws cognito authentication line
    
    Hadith corrections archive table added for approved/deleted hadiths
    
    Archive now stores username too

--- frontend/config.py
+++ frontend/config.py
@@ -7,6 +7,7 @@ class Config(object):
     JSON_SORT_KEYS = False
     DYNAMODB_ENDPOINT_URL = '{DYNAMODB_ENDPOINT_URL}'.format(**os.environ)
     DYNAMODB_TABLE = '{DYNAMODB_TABLE}'.format(**os.environ)
+    DYNAMODB_TABLE_ARCHIVE = '{DYNAMODB_TABLE_ARCHIVE}'.format(**os.environ)
 
     AWS_ACCESS_KEY_ID = '{AWS_ACCESS_KEY_ID}'.format(**os.environ)
     AWS_SECRET_ACCESS_KEY = '{AWS_SECRET_ACCESS_KEY}'.format(**os.environ)

bbf9dc3d99d55d7989276eb82ad125b639cad09e
Hidayath Ansari <ansari@spokesly.com>
2021-03-02 15:05:58 -0800

    Returning a string depending on whether DDB persist succeeded or not

--- backend/hadith-ingestor.js
+++ backend/hadith-ingestor.js
@@ -5,15 +5,23 @@ const dynamo = new AWS.DynamoDB.DocumentClient();
 
 exports.handler = async (event, context) => {
     console.log('Received event:', JSON.stringify(event, null, 2));
-
+    
     const payload = {
         TableName: 'HadithCorrections',
-        Item: JSON.parse(event.body)
+        Item: JSON.parse(event)
     };
 
     payload.Item.id = new Date().getTime() + ':' + context.awsRequestId;
     payload.Item.lastAssigned = null;
     payload.Item.version = 0;
     
-    return await dynamo.put(payload).promise();
+    return await dynamo.put(payload).promise().then(
+        function(data) {
+            return "Success";
+        },
+        function(error) {
+            return "An error occurred";
+        }
+    );
 };
+
